9715de53fe5baeb73803f97f12ee1193492d92aa
Oliver <duel80003@gmail.com>
2022-12-24 10:15:02 +0800

    init

---
+++ log.go
@@ -0,0 +1,38 @@
+package tools
+
+import (
+	"fmt"
+	"github.com/sirupsen/logrus"
+	"os"
+	"strings"
+)
+
+const (
+	DebugLevel = "debug"
+)
+
+var (
+	Logger = logrus.New()
+)
+
+type MyTextFormatter struct{}
+
+func (t *MyTextFormatter) Format(entry *logrus.Entry) ([]byte, error) {
+	timeStr := fmt.Sprintf("%d/%02d/%02d %02d:%02d:%02d", entry.Time.Year(), entry.Time.Month(), entry.Time.Day(), entry.Time.Hour(), entry.Time.Minute(), entry.Time.Second())
+	filePath := strings.Split(entry.Caller.File, "/")
+	directName := filePath[len(filePath)-2]
+	name := filePath[len(filePath)-1]
+	result := fmt.Sprintf("[%s] [%s/%s:%d] [%s] %s", timeStr, directName, name, entry.Caller.Line, entry.Level, entry.Message)
+	return append([]byte(result), '\n'), nil
+}
+
+func LogInit() {
+	Logger.SetFormatter(&MyTextFormatter{})
+	Logger.Out = os.Stdout
+	Logger.SetLevel(logrus.InfoLevel)
+	env := os.Getenv("LOG_LEVEL")
+	if strings.ToLower(env) == DebugLevel {
+		Logger.SetLevel(logrus.DebugLevel)
+	}
+	Logger.SetReportCaller(true)
+}
